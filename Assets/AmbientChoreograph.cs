f069287be023323b5a2c976e04ff6a64306f8bde
stoppelkopp <stoppelwoppel@gmail.com>
2016-10-22 16:57:22 +0200

    Added cool ambient audio

---
+++ Assets/AmbientChoreograph.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientChoreograph : MonoBehaviour {
+
+    private AudioSource ambient1;
+    private AudioSource ambient2;
+    private AudioSource welcome;
+    private AudioSource goodbye;
+
+    void Awake()
+    {
+        ambient1 = GetComponents<AudioSource>()[0];
+        ambient2 = GetComponents<AudioSource>()[1];
+        welcome = GetComponents<AudioSource>()[2];
+        goodbye = GetComponents<AudioSource>()[3];
+
+        LifeCycle lifeCycle = FindObjectOfType<LifeCycle>();
+        if (lifeCycle != null)
+        {
+            lifeCycle.OnBeginHandler += OnBegin;
+            lifeCycle.OnFinishHandler += OnFinish;
+        }
+    }
+
+    private void OnBegin()
+    {
+        // todo Fading
+        ambient2.volume = 1;
+        welcome.Play();
+    }
+
+
+    private void OnFinish()
+    {
+        // todo Fading
+        ambient2.volume = 0;
+        goodbye.Play();
+    }
+}
---
+++ Assets/DestroyOnBegin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnBegin : MonoBehaviour {
+
+    void Awake()
+    {
+        LifeCycle lifeCycle = FindObjectOfType<LifeCycle>();
+        lifeCycle.OnBeginHandler += OnBegin;
+    }
+
+    private void OnBegin()
+    {
+        LifeCycle lifeCycle = FindObjectOfType<LifeCycle>();
+        lifeCycle.OnBeginHandler -= OnBegin;
+        GameObject.Destroy(gameObject);
+    }
+}
