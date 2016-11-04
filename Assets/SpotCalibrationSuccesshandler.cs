33da756525bbcbab5e243cf22c40c9141ddd8f27
Your Name <you@example.com>
2016-11-04 19:01:03 +0100

    Interaction origin

---
+++ Assets/SpotCalibrationSuccesshandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotCalibrationSuccesshandler : MonoBehaviour {
+
+    void Awake()
+    {
+        LifeCycle lifeCycle = FindObjectOfType<LifeCycle>();
+        if (lifeCycle != null)
+        {
+            lifeCycle.OnInitHandler += OnInit;
+        }
+    }
+
+    private void OnInit()
+    {
+        FindObjectOfType<TableCalibrator>().OnCalibrationCompleteHandler += OnCalibrationComplete;
+    }
+
+    private void OnCalibrationComplete(Vector3 center, float radius)
+    {
+        transform.position = new Vector3(center.x, 0, center.z);
+    }
+}
