033b29a21f38bb83f364d7197dcbc542ca6b8af0
stoppelkopp <stoppelwoppel@gmail.com>
2016-11-17 00:03:29 +0100

    selection

---
+++ Assets/Player/BuildingSelection/LookAtBuildingHandler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtBuildingHandler : MonoBehaviour {
+
+    private Ray ray;
+    private float timeNextSample;
+    private Building lastSelectedBuilding;
+    private GameObject currentSelectionObject;
+
+    public float samplingIntervalInSeconds = 0.25f;
+    public GameObject selectionPrefab;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > timeNextSample)
+        {
+            ray.origin = transform.position;
+            ray.direction = transform.forward;
+            timeNextSample = Time.time + samplingIntervalInSeconds;
+
+            float nearestDist = float.MaxValue;
+            Building selectedBuilding = null;
+
+            foreach (RaycastHit hit in Physics.RaycastAll(ray, 100))
+            {
+                if (hit.distance > nearestDist)
+                {
+                    continue;
+                }
+                nearestDist = hit.distance;
+                if (hit.collider.transform.GetComponent<Building>() != null)
+                {
+                    selectedBuilding = hit.collider.transform.GetComponent<Building>();
+                }
+            }
+            if (selectedBuilding != lastSelectedBuilding)
+            {
+                if (selectedBuilding != null)
+                {
+                    transform.FindChild("FileNameLabel").GetComponent<TextMesh>().text = selectedBuilding.node.name;
+                    transform.FindChild("PathNameLabel").GetComponent<TextMesh>().text = selectedBuilding.node.pathName;
+                    if (currentSelectionObject != null)
+                    {
+                        Destroy(currentSelectionObject);
+                    }
+                    currentSelectionObject = (GameObject) GameObject.Instantiate(selectionPrefab);
+                    Bounds bounds = selectedBuilding.gameObject.GetComponent<Renderer>().bounds;
+                    currentSelectionObject.transform.position = bounds.center;
+                    currentSelectionObject.transform.localScale = bounds.size + new Vector3(0.001f, 0.001f, 0.001f);
+                }
+                else
+                {
+                    transform.FindChild("FileNameLabel").GetComponent<TextMesh>().text = "";
+                    transform.FindChild("PathNameLabel").GetComponent<TextMesh>().text = "";
+                    if (currentSelectionObject != null)
+                    {
+                        Destroy(currentSelectionObject);
+                        currentSelectionObject = null;
+                    }
+                }
+                lastSelectedBuilding = selectedBuilding;
+            }
+        }
+	}
+}
---
+++ Assets/Player/BuildingSelection/SelectionShader.shader
@@ -0,0 +1,37 @@
+﻿Shader "Custom/SelectionShader" {
+Properties{
+	_Color("Color", Color) = (1,1,1,1)
+	_MainTex("Base (RGB)", 2D) = "	" {}
+}
+SubShader{
+	Tags{ "RenderType" = "Fade" }
+
+	CGPROGRAM
+	// Physically based Standard lighting model, and enable shadows on all light types
+#pragma surface surf Lambert alpha 
+
+	// Use shader model 3.0 target, to get nicer looking lighting
+#pragma target 3.0
+
+
+struct Input {
+	float3 viewDir;
+	float3 worldPos;
+};
+
+
+fixed4 _Color;
+
+void surf(Input IN, inout SurfaceOutput o) {
+
+
+	float c = ((sin(_Time[1] * 3) + 1) / 2) * 0.5 + 0.3;
+
+
+	o.Alpha = c;
+	o.Emission = _Color*c;
+}
+ENDCG
+}
+FallBack "Diffuse"
+}
