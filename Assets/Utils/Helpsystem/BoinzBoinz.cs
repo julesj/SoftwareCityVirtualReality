8d06c9b2a1b542534d84b9eecab550c69fe50722
Your Name <you@example.com>
2016-12-21 16:19:23 +0100

    Boinz + haptic

--- Assets/Utils/Helpsystem/BoinzBoinz.cs
+++ Assets/Utils/Helpsystem/BoinzBoinz.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using VRTK;
 
 public class BoinzBoinz : MonoBehaviour {
 
     private Vector3 startOffset;
+
+    public bool hapticLeft = false;
+    public bool hapticRight = false;
+    public float hapticStrength = 0.1f;
+
     public float boinzHeight = 0.25f;
     public float boinzSpeed = 1;
+
+    private float lastBoinzHeight;
 
     void Start()
     {
@@ -13,7 +21,23 @@ public class BoinzBoinz : MonoBehaviour {
     }
 	// Update is called once per frame
 	void Update () {
-        transform.localPosition = startOffset + new Vector3(0, Mathf.Abs(Mathf.Sin(Time.time * Mathf.PI * boinzSpeed)) * boinzHeight, 0);
+        float height = Mathf.Sin(Time.time * Mathf.PI * boinzSpeed) * boinzHeight;
+
+        if (Mathf.Sign(height) != Mathf.Sign(lastBoinzHeight))
+        {
+            if (hapticLeft)
+            {
+                VRTK_DeviceFinder.GetControllerLeftHand().GetComponent<VRTK_ControllerActions>().TriggerHapticPulse((ushort)(hapticStrength * 3999));
+            }
+            if (hapticRight)
+            {
+                VRTK_DeviceFinder.GetControllerRightHand().GetComponent<VRTK_ControllerActions>().TriggerHapticPulse((ushort)(hapticStrength * 3999));
+            }
+        }
+        lastBoinzHeight = height;
+
+        transform.localPosition = startOffset + new Vector3(0, Mathf.Abs(height), 0);
         transform.localRotation = Quaternion.AxisAngle(Vector3.up, Time.time);
+
 	}
 }
