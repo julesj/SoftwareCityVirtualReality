d486d9c5cca6aed58056228361c413971c5cb326
Barbara Streppel <barbara.streppel@maibornwolff.de>
2017-02-06 17:28:26 +0100

    WelcomeScene

---
+++ Assets/WelcomeScenes/ClickListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClickListener : MonoBehaviour {
+
+    Button ok;
+
+	// Use this for initialization
+	void Start () {
+        ok = GameObject.Find("OK_Button").GetComponent<Button>();
+	}
+	
+    public void GoTo(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
