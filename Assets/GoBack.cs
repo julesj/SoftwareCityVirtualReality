5813ca5e7395f70eadd684c81dff316d7cb69473
Barbara Streppel <barbara.streppel@maibornwolff.de>
2017-02-07 17:42:03 +0100

    New Skyboxes and reangarrangement of Scenes and Prefabs

---
+++ Assets/GoBack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VRTK
+{
+    public class GoBack : MonoBehaviour
+    {
+
+        SteamVR_TrackedObject trackedObj;
+        SteamVR_Controller.Device device;
+        Vector2 axis;
+        float angle;
+        ClipboardBar clipboard;
+
+        // Use this for initialization
+        void Awake()
+        {
+            trackedObj = GetComponent<SteamVR_TrackedObject>();
+        }
+
+        void Start()
+        {
+            GetComponent<VRTK_ControllerEvents>().TouchpadAxisChanged += new ControllerInteractionEventHandler(DoTouchpadAxisChanged);
+            GetComponent<VRTK_ControllerEvents>().TouchpadPressed += new ControllerInteractionEventHandler(DoTouchpadPressed);
+            clipboard = GameObject.Find("Main").GetComponent<ClipboardBar>();
+        }
+
+        void DoTouchpadAxisChanged(object sender, ControllerInteractionEventArgs e)
+        {
+            axis = e.touchpadAxis;
+            angle = e.touchpadAngle;
+        }
+
+        void DoTouchpadPressed(object sender, ControllerInteractionEventArgs e)
+        {
+            if (angle > 180)
+            {
+                Debug.Log("before: " + clipboard.getBefore());
+                SceneManager.LoadScene(clipboard.getBefore());
+            }
+        }
+    }
+}
