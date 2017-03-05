ea6b87842f009366820056774372cbe53838beff
julesj <foo@rz.fh-augsburg.de>
2017-03-05 13:28:00 +0100

    added example scene for scaling and rotating

---
+++ Assets/ScaleRotateExampleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleRotateExampleScript : MonoBehaviour {
+
+    private FloatModel scaleModel;
+    private FloatModel rotateModel;
+
+    public float scale;
+    public float rotation;
+    
+    void Start () {
+        foreach (FloatModel model in FindObjectsOfType<FloatModel>())
+        {
+            if (model.name.Equals("Scale"))
+            {
+                scaleModel = model;
+            }
+            else if (model.name.Equals("Rotate"))
+            {
+                rotateModel = model;
+            }
+        }
+    }
+	
+	void Update () {
+        scaleModel.SetValue(scale);
+        rotateModel.SetValue(rotation);
+	}
+}
