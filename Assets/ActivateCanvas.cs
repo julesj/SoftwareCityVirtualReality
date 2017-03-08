75c0f313839f5e98e7f7e0395ba367e1a7eff536
Barbara Streppel <barbara.streppel@maibornwolff.de>
2017-03-08 17:18:27 +0100

    Created UI for Mixed Control

---
+++ Assets/ActivateCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActivateCanvas : MonoBehaviour {
+    GameObject panel;
+
+    void Start()
+    {
+        SceneManager.sceneLoaded += SceneLoaded;
+        panel = GameObject.Find("UI_Panel");
+        panel.SetActive(false);
+    }
+
+    void SceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "ScaleRotateExampleScene")
+        {
+            panel.SetActive(true);
+        }
+    }
+}
---
+++ Assets/PlayerScenes/ControlValue.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRTK;
+using VRTK.UnityEventHelper;
+using UnityEngine.SceneManagement;
+
+public class ControlValue : MonoBehaviour {
+
+    public Text go;
+
+    private VRTK_Control_UnityEvents controlEvents;
+    private FloatModel scaleModel;
+
+    private void Start()
+    {
+        controlEvents = GetComponent<VRTK_Control_UnityEvents>();
+        if (controlEvents == null)
+        {
+            controlEvents = gameObject.AddComponent<VRTK_Control_UnityEvents>();
+        }
+
+        controlEvents.OnValueChanged.AddListener(HandleChange);
+
+        foreach (FloatModel model in FindObjectsOfType<FloatModel>())
+        {
+            if (model.name.Equals("Scale"))
+            {
+                scaleModel = model;
+            }
+        }
+
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
+
+    void SceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Szene " + scene.name + " wurde geladen");
+        if (scene.name == "ScaleRotateExampleScene")
+        {
+            foreach (FloatModel model in FindObjectsOfType<FloatModel>())
+            {
+                if (model.name.Equals("Scale"))
+                {
+                    scaleModel = model;
+                }
+            }
+        }
+    }
+
+    private void HandleChange(object sender, Control3DEventArgs e)
+    {
+        go.text = e.value.ToString() + "(" + e.normalizedValue.ToString() + "%)";
+        scaleModel.SetValue(e.value / 100);
+    }
+}
